113f7beccdf18fe618b8a9cb8e63e3534b1842ac
RahulDMello <rvdmello96@gmail.com>
2021-02-18 05:00:14 -0500

    Add flow 1

--- NavigationDemo/app/src/main/java/com/example/navigationdemo/MainActivity.kt
+++ NavigationDemo/app/src/main/java/com/example/navigationdemo/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.navigationdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.FragmentContainerView
+import androidx.fragment.app.findFragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.*
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+        val topLevelDestinations = setOf(
+            R.id.mainFragment,
+            R.id.flow1Fragment1
+        )
+        appBarConfiguration = AppBarConfiguration(topLevelDestinations, findViewById<DrawerLayout>(R.id.drawer_layout))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        findViewById<NavigationView>(R.id.nav_view)
+            .setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 }
